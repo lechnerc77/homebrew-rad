703e9b4205110177cc532470d5200959e08682f2
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-01-05 12:15:47 +0100

    urknall
    Signed-off-by: Christian Lechner <lechnerc77@users.noreply.github.com>

---
+++ Formula/rad.rb
@@ -0,0 +1,33 @@
+class Rad < Formula
+    desc "Radius CLI"
+    homepage "https://github.com/radius-project/radius"
+    
+    if Hardware::CPU.intel?
+      url "https://github.com/radius-project/radius/releases/download/v0.28.0/rad_darwin_amd64"
+      sha256 "619776c81240c0bb2ea3409a36f8d4e319f5bb48d46b40f237978f5eb932d449"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/radius-project/radius/releases/download/v0.28.0/rad_darwin_arm64"
+      sha256 "175e7db5f53f28fceafb50150288255cc0e6fd06004b69d2dbd691827ec0a242"
+    end
+  
+    version "0.28.0"
+    
+    license "MIT"
+  
+    def install
+      if Hardware::CPU.intel?
+        bin.install "rad_darwin_amd64" => "rad"
+      elsif Hardware::CPU.arm?
+        bin.install "rad_darwin_arm64" => "rad"
+      end
+  
+      (bin/".installed-by.txt").write "brew"
+    end
+  
+    test do
+      version_output = shell_output "#{bin}/rad version"
+      assert_equal 0, $CHILD_STATUS.exitstatus
+      assert_match "rad version", version_output
+    end
+  end
+  
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# homebrew-rad
-Unofficial Homebrew Tap for Radius CLI (rad)
+# Homebrew Tap for Radius CLI
+
+This is the **unofficial* Homebrew Tap for Radius CLI (rad).
+
+To learn more about Radius check out the following resources:
+
+* Project page for [Radius](https://radapp.io/)
+* [GitHub Repository](https://github.com/radius-project/radius)
