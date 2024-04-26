e7b5d0ba12e56d032699104864946e81a5110c09
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-04-26 12:28:41 +0200

    chore: update to version 0.33.0

--- Formula/rad.rb
+++ Formula/rad.rb
@@ -3,14 +3,14 @@ class Rad < Formula
     homepage "https://github.com/radius-project/radius"
     
     if Hardware::CPU.intel?
-      url "https://github.com/radius-project/radius/releases/download/v0.32.0/rad_darwin_amd64"
-      sha256 "3180398e23e52898c5c35b2a29fea8be33a6956a79a757d7f5b90c586903d75d"
+      url "https://github.com/radius-project/radius/releases/download/v0.33.0/rad_darwin_amd64"
+      sha256 "309dfbd901b12997b0aeb2aeb51649f078481a754f64a71c784ce04b8bdc5851"
     elsif Hardware::CPU.arm?
-      url "https://github.com/radius-project/radius/releases/download/v0.32.0/rad_darwin_arm64"
-      sha256 "971c70bf2309bf5efce365362f020428598edf036bba3da0aa6580a72b0436de"
+      url "https://github.com/radius-project/radius/releases/download/v0.33.0/rad_darwin_arm64"
+      sha256 "74e8117253c1b15430f02f462fdb3dc98a5d3d80240797732c7f2010f3b47075"
     end
   
-    version "0.32.0"
+    version "0.33.0"
     
     license "Apache 2.0"
   
